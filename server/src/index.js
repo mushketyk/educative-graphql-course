4f49711cac77b0f234071c4144cd5dba604ed8d7
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2021-07-29 22:22:21 +0100

    [Server - 2] GraphQL resolver

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,12 @@
 const { ApolloServer } = require('apollo-server')
 const { readSchema } = require('./schema.js')
 const { resolvers } = require('./resolvers.js')
-const {ApolloServerPluginLandingPageGraphQLPlayground} = require('apollo-server-core')
 
 const typeDefs = readSchema()
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground(),
-  ],
 })
 
 server.listen().then(() => {
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,10 +1,38 @@
+const productsData = [
+  {
+    name: 'Educative',
+    description: 'Interactive Courses for Software Developers',
+    url: 'https://educative.io/',
+    numberOfVotes: 10,
+    publishedAt: '2021-04-05',
+  },
+  {
+    name: 'Apollo',
+    description: 'The Apollo Data Graph Platform',
+    url: 'https://www.apollographql.com/',
+    numberOfVotes: 5,
+    publishedAt: '2021-01-08',
+  },
+  {
+    name: 'OneGraph',
+    description: 'Build Integrations 100x Faster',
+    url: 'https://www.onegraph.com',
+    numberOfVotes: 5,
+    publishedAt: '2020-08-22',
+  },
+]
+
+
 const resolvers = {
   Query: {
-    appName: () =>
-      'ProductHunt clone'
+    appName: () => 'ProductHunt clone',
+
+    allProducts: () => {
+      return productsData
+    },
   },
 }
 
 module.exports = {
-  resolvers
+  resolvers,
 }
