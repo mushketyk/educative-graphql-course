e14ccdbd4798af70ae52afca780a116743f98415
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2021-08-04 22:29:30 +0100

    [Exercise - set up] Categories set up

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -6,6 +6,7 @@ const productsData = [
     numberOfVotes: 10,
     publishedAt: '2021-04-05',
     authorId: '1',
+    categoriesIds: ['1']
   },
   {
     name: 'Apollo',
@@ -14,6 +15,7 @@ const productsData = [
     numberOfVotes: 5,
     publishedAt: '2021-01-08',
     authorId: '2',
+    categoriesIds: ['2', '3']
   },
   {
     name: 'OneGraph',
@@ -22,6 +24,7 @@ const productsData = [
     numberOfVotes: 5,
     publishedAt: '2020-08-22',
     authorId: '1',
+    categoriesIds: ['3']
   },
 ]
 
@@ -38,6 +41,24 @@ const usersData = [
   },
 ]
 
+const categoriesData = [
+  {
+    id: '1',
+    slug: 'education',
+    name: 'Education',
+  },
+  {
+    id: '2',
+    slug: 'frameworks',
+    name: 'Frameworks',
+  },
+  {
+    id: '3',
+    slug: 'api',
+    name: 'API',
+  },
+]
+
 const resolvers = {
   Query: {
     appName: () => 'ProductHunt clone',
@@ -49,12 +70,16 @@ const resolvers = {
       const user = usersData.find(user => user.userName === authorName)
       return productsData.filter(product => product.authorId === user.id)
     }
+
+    // TODO: Implement new query
   },
 
   Product: {
     author: (product) => {
       return usersData.find(user => user.id === product.authorId)
     },
+
+    // TODO: Implement categories resolver
   },
 }
 
