c1eae194476a0ebbe41209593ba667526b09efa9
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2021-08-04 21:13:11 +0100

    [Exercise set up] Integrate user resolvers to a DB

---
+++ server/src/models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+  // TODO: Define Mongo schema
+})
+
+module.exports = mongoose.model('User', UserSchema)
