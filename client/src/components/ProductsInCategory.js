df0a7f433125f94f645dc3165482cd14ebe0f69d
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2021-09-25 22:47:42 +0100

    [Client][Exercise] - Solution

--- client/src/components/ProductsInCategory.js
+++ client/src/components/ProductsInCategory.js
@@ -1,23 +1,33 @@
-import React from 'react'
+import { useQuery } from '@apollo/client'
 import Typography from '@mui/material/Typography'
-import ProductsList from './ProductsList'
-import allProducts from './test-data'
-
+import React from 'react'
 import {
   useParams
 } from 'react-router-dom'
+import ProductsList from './ProductsList'
+import { GET_PRODUCTS_IN_CATEGORY } from './queries'
+
 
 export default function ProductsInCategory() {
   const { slug } = useParams()
 
+  const {
+    data,
+    loading,
+    error,
+    refetch
+  } = useQuery(GET_PRODUCTS_IN_CATEGORY, {
+    variables: { categorySlug: slug },
+  })
 
-  // TODO: Fetch data here using the "useQuery" hook
-
-  // Update a list of properties passed to the "ProductsList"
   return (
     <>
       <Typography variant="h3">Products by in category {slug}</Typography>
-      <ProductsList products={allProducts} />
+      <ProductsList
+        products={data?.productsByCategory || []}
+        loading={loading}
+        error={error}
+        refetch={refetch} />
     </>
   )
 }
