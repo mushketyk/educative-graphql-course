c9a399d3c9c7a36595163e1cb6a0d45329393fcd
Ivan Mushketyk <ivan.mushketyk@gmail.com>
2021-09-25 22:33:18 +0100

    [client] Query parameters

--- client/src/components/ProductsByUser.js
+++ client/src/components/ProductsByUser.js
@@ -1,19 +1,55 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import ProductsList from './ProductsList'
-import allProducts from './test-data'
+import { gql, useQuery } from '@apollo/client'
 
 import {
   useParams
 } from 'react-router-dom'
 
+const GET_PRODUCTS_BY_AUTHOR = gql`
+query($authorName: String!) {
+  productsByAuthor(authorName: $authorName) {
+    id
+    description
+    name
+    url
+    numberOfVotes
+    publishedAt
+    author {
+      id
+      userName
+      fullName
+    }
+    categories {
+      id
+      slug
+      name
+    }
+  }
+}
+`
+
 export default function ProductsByUser() {
   const { userName } = useParams()
+
+  const {
+    data,
+    loading,
+    error,
+    refetch
+  } = useQuery(GET_PRODUCTS_BY_AUTHOR, {
+    variables: { authorName: userName },
+  })
 
   return (
     <>
       <Typography variant="h3">Products by user {userName}</Typography>
-      <ProductsList products={allProducts} />
+      <ProductsList
+        products={data?.productsByAuthor || []}
+        loading={loading}
+        error={error}
+        refetch={refetch} />
     </>
   )
 }
